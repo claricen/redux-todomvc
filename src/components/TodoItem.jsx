39ebea58be315d2f3979e7420d182f6a3e3500ed
Clarice Ng <clare.ngx@gmail.com>
2017-06-08 14:48:53 -0700

    pure mixins working

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 import TextInput from './TextInput';
 
 export default class TodoItem extends React.Component {
