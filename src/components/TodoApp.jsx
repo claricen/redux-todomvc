526e0fad801e82e3af63d09f4da5dc704d271415
Clarice Ng <clare.ngx@gmail.com>
2017-06-09 09:58:21 -0700

    begin redux

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -3,8 +3,9 @@ import TodoList from './TodoList'
 import TodoHeader from './TodoHeader'
 import TodoTools from './TodoTools'
 import Footer from './Footer'
+import {connect} from 'react-redux';
 
-export default class TodoApp extends React.Component {
+export class TodoApp extends React.Component {
   getNbActiveItems() {
     if (this.props.todos) {
       const activeItems = this.props.todos.filter(
@@ -26,3 +27,12 @@ export default class TodoApp extends React.Component {
     </div>
   }
 };
+
+function mapStateToProps(state) {
+  return {
+    todos: state.get('todos'),
+    filter: state.get('filter')
+  };
+}
+
+export const TodoAppContainer = connect(mapStateToProps)(TodoApp);
