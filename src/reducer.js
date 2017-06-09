71187b927f9ed9352703ab150b4b2cb6292502a9
Clarice Ng <clare.ngx@gmail.com>
2017-06-09 11:00:58 -0700

    adds editing feature

--- src/reducer.js
+++ src/reducer.js
@@ -4,14 +4,45 @@ function setState(state, newState) {
   return state.merge(newState);
 }
 
-function toggleComplete(state, itemId) {
-  const itemIndex = state.get('todos').findIndex(
+function findItemIndex(state, itemId) {
+  return state.get('todos').findIndex(
     (item) => item.get('id') === itemId
   );
+}
 
+function toggleComplete(state, itemId) {
+  const itemIndex = findItemIndex(state, itemId);
   const updatedItem = state.get('todos')
     .get(itemIndex)
     .update('status', status => status === 'active' ? 'completed' : 'active');
+
+  return state.update('todos', todos => todos.set(itemIndex, updatedItem));
+}
+
+function editItem(state, itemId) {
+  const itemIndex = findItemIndex(state, itemId);
+  const updatedItem = state.get('todos')
+    .get(itemIndex)
+    .set('editing', true);
+
+  return state.update('todos', todos => todos.set(itemIndex, updatedItem));
+}
+
+function cancelEditing(state, itemId) {
+  const itemIndex = findItemIndex(state, itemId);
+  const updatedItem = state.get('todos')
+    .get(itemIndex)
+    .set('editing', false);
+
+  return state.update('todos', todos => todos.set(itemIndex, updatedItem));
+}
+
+function doneEditing(state, itemId, newText) {
+  const itemIndex = findItemIndex(state, itemId);
+  const updatedItem = state.get('todos')
+    .get(itemIndex)
+    .set('editing', false)
+    .set('text', newText);
 
   return state.update('todos', todos => todos.set(itemIndex, updatedItem));
 }
@@ -28,6 +59,12 @@ export default function(state = Map(), action) {
       return toggleComplete(state, action.itemId);
     case 'CHANGE_FILTER':
       return changeFilter(state, action.filter);
+    case 'EDIT_ITEM':
+      return editItem(state, action.itemId);
+    case 'CANCEL_EDITING':
+      return cancelEditing(state, action.itemId);
+    case 'DONE_EDITING':
+      return doneEditing(state, action.itemId, action.newText);
   }
   return state;
 }
